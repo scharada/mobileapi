b6363cbf215f0f0cd0df1c3d9243c274591cab75
mathias.dietrich@gmail.com <mathias.dietrich@gmail.com@2e27937c-4af7-273e-8a7e-c69e31eec246>
2013-06-12 06:00:33 +0000

    init

---
+++ trunk/src/VS/server/org.mobileapi.server.windows.shared/Key.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace org.mobileapi.server.windows.shared
+{
+    public class Key
+    {
+        public const string ADMINID = "adminid";
+        public const string APPLICATIONID = "appid";
+        public const string BINARY = "bin";
+        public const string BODYLENGTH = "blen";
+        public const string CREATED = "c";
+        public const string COMPANYID = "comid";
+        public const string CHECKSUM = "crc";
+        public const string DEVICEID = "devid";
+        public const string EMAIL = "email";
+        public const string ENCODING = "enc";
+        public const string FIX = "fix";
+        public const string GROUPID = "grpid";
+        public const string HEADERLENGTH = "hlen";
+        public const string LASTGPSTIME = "lgpst";
+        public const string LASTLOGGEDOFF = "lloff";
+        public const string LASTLOGGEDON = "llon";
+        public const string LATITUDE = "lat";
+        public const string LONGITUDE = "long";
+        public const string MESSAGE = "msg";
+        public const string MESSAGEID = "msgid";
+        public const string NAME = "name";
+        public const string OBJECT = "obj";
+        public const string OPCODE = "opc";
+        public const string PARTS = "parts";
+        public const string PERMISSION = "perm";
+        public const string PIN = "pin";
+        public const string POSITION = "pos";
+        public const string PRIORITY = "prio";
+        public const string PASSWORT = "pwd";
+        public const string RECEIVER = "rec";
+        public const string RETRIES = "ret";
+        public const string SATELLITES = "sat";
+        public const string SEQUENCE = "seq";
+        public const string SENDER = "sndr";
+        public const string TOKEN = "tkn";
+        public const string NAME = "ttl";
+        public const string UPDATED = "u";
+        public const string VERSION = "vers";
+    }
+}
