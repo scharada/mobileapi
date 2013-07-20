4efab7b582092fb81699eb84c738e75b5ce5efb5
mathias.dietrich@gmail.com <mathias.dietrich@gmail.com@2e27937c-4af7-273e-8a7e-c69e31eec246>
2013-07-20 08:38:22 +0000

    chat example

--- trunk/src/Java/org.mobileapi.client.android.example/src/com/example/org/mobileapi/client/android/example/MainActivity.java
+++ trunk/src/Java/org.mobileapi.client.android.example/src/com/example/org/mobileapi/client/android/example/MainActivity.java
@@ -4,24 +4,35 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
-
 import org.apache.http.message.BasicNameValuePair;
-
 import com.codebutler.android_websockets.WebSocketClient;
 import com.codebutler.android_websockets.WebSocketClient.Listener;
-
-import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.app.Activity;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity implements Listener {
 	
-	List<BasicNameValuePair> header = new ArrayList<BasicNameValuePair>();
+	public static final String GATEWAY = "ws://gate.mobileapi.org:8080/org.mobileapi.server.api/portal";
 	
-	String TAG = "MainActivity";
-	WebSocketClient client ;
+	private String TAG = "MainActivity";
+	private static Handler mHandler;
+	 
+	private WebSocketClient client ;
+	private List<BasicNameValuePair> header = new ArrayList<BasicNameValuePair>();
+	
+	private String messages = "";
+	private EditText tbxMessage;
+	private TextView tbxMessages;
+	private TextView btnSend;
+	private TextView btnClear;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -29,9 +40,23 @@ public class MainActivity extends Activity implements Listener {
 		
 		setContentView(R.layout.activity_main);
 		
+		tbxMessage = (EditText)findViewById(R.id.tbxMsg);
+		tbxMessages = (TextView)findViewById(R.id.tbxMessages);
+		btnSend = (TextView)findViewById(R.id.btnSend);
+		btnClear = (TextView)findViewById(R.id.btnClear);
+		
+		 mHandler = new Handler(Looper.getMainLooper()) {
+			 @Override
+	            public void handleMessage(Message inputMessage) {
+				 tbxMessages.setText(messages);
+				 
+				 super.handleMessage(inputMessage);
+			 }
+		 };
+				
 		URI uri = null;
 		try {
-			uri = new URI("ws://etiamo.com:8080/org.mobileapi.server.api/portal");
+			uri = new URI(GATEWAY);
 		} catch (URISyntaxException e) {
 
 			Log.e(TAG, e.getMessage());
@@ -52,13 +77,14 @@ public class MainActivity extends Activity implements Listener {
 
     public void onConnect()
     {
-    	Log.d(TAG, "onConnect");
+    	addText("Connected");
     	client.send("Hello from Android");
     }
     
     public void onMessage(String message)
     {
-    	Log.d(TAG, "onMessage");
+    	Log.d(TAG, "onMessage " + message);
+    	 addText(message);
     }
     
     public void onMessage(byte[] data)
@@ -74,5 +100,23 @@ public class MainActivity extends Activity implements Listener {
     public void onError(Exception error)
     {
     	Log.d(TAG, "onError " + error.getMessage());
+    }
+    
+    public void onClear(View v)
+    {
+    	messages = "";
+    	mHandler.sendEmptyMessage(0);
+    }
+    
+    public void onSend(View v)
+    {
+    	String msg = tbxMessage.getText().toString();
+    	client.send(msg);
+    }
+    
+    public void addText(String msg)
+    {
+    	messages = msg + "\r\n"  + messages;
+    	mHandler.sendEmptyMessage(0);
     }
 }
